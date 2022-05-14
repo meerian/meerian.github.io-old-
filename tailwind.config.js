79ee4fa59f10be8b62585255482e3098f18f112f
meerian <myronneozx@gmail.com>
2022-05-15 00:09:47 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: {
+          50: '#FCFBE4'
+        },
+        green: {
+          50: '#235823'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
